8481d094b892ed66c7bcc18c7c5c1f5a06515e2e
tan-ziyu <tan_ziyu@apple.com>
2019-03-01 17:25:20 +0800

    delegration pattern

--- design pattern demo/Controller/ViewController.swift
+++ design pattern demo/Controller/ViewController.swift
@@ -40,48 +40,48 @@ final class ViewController: UIViewController {
   @IBOutlet var trashBarButtonItem: UIBarButtonItem!
   
   // Decorator pattern -- Delegration:
-//  private var currentAlbumIndex = 0
-//  private var currentAlbumData: [AlbumData]?
-//  private var allAlbums = [Album]()
+  private var currentAlbumIndex = 0
+  private var currentAlbumData: [AlbumData]?
+  private var allAlbums = [Album]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-//    allAlbums = LibraryAPI.shared.getAlbums()
-//    tableView.dataSource = self
-//
-//    showDataForAlbum(at: currentAlbumIndex)
+    allAlbums = LibraryAPI.shared.getAlbums()
+    tableView.dataSource = self
+
+    showDataForAlbum(at: currentAlbumIndex)
   }
   
-//  private func showDataForAlbum(at index: Int) {
-//    if index < allAlbums.count && index > -1 {
-//      let album = allAlbums[index]
-//      currentAlbumData = album.tableRepresentation
-//    } else {
-//      currentAlbumData = nil
-//    }
-//    tableView.reloadData()
-//  }
+  private func showDataForAlbum(at index: Int) {
+    if index < allAlbums.count && index > -1 {
+      let album = allAlbums[index]
+      currentAlbumData = album.tableRepresentation
+    } else {
+      currentAlbumData = nil
+    }
+    tableView.reloadData()
+  }
 
 }
 
-//extension ViewController: UITableViewDataSource {
-//  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    guard let albumData = currentAlbumData else {
-//      return 0
-//    }
-//    return albumData.count
-//  }
-//
-//  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//    let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifier, for: indexPath)
-//    if let albumData = currentAlbumData {
-//      let row = indexPath.row
-//      cell.textLabel?.text = albumData[row].title
-//      cell.detailTextLabel?.text = albumData[row].value
-//    }
-//    return cell
-//  }
-//}
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let albumData = currentAlbumData else {
+      return 0
+    }
+    return albumData.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifier, for: indexPath)
+    if let albumData = currentAlbumData {
+      let row = indexPath.row
+      cell.textLabel?.text = albumData[row].title
+      cell.detailTextLabel?.text = albumData[row].value
+    }
+    return cell
+  }
+}
 
