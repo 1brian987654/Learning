869f88d216e4954610954495263de71fcacf9a21
tan-ziyu <tan_ziyu@apple.com>
2019-03-26 18:49:10 +0800

    add design patterns

---
+++ LearningDesignPatterns/Design_Patterns_by_Tutorials_v1/05-strategy-pattern/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+/*:
+ [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
+ 
+ # Delegation
+ - - - - - - - - - -
+ ![Delegation Diagram](Delegation_Diagram.png)
+ 
+ The delegation pattern allows an object to use a helper object to perform a task, instead of doing the task itself.
+ 
+ This allows for code reuse through object composition, instead of inheritance.
+ 
+ ## Code Example
+ */
+import UIKit
+
+protocol MenuViewControllerDelegate {
+  func menuViewController(_ menuViewController: MenuViewController,
+                          didSelectItemAtIndex index: Int)
+}
+
+class MenuViewController: UIViewController {
+  
+  // 1
+  var delegate: MenuViewControllerDelegate?
+  @IBOutlet var tableView: UITableView! {
+    didSet {
+      tableView.dataSource = self
+      tableView.delegate = self
+    }
+  }
+  
+  // 2
+  let items = ["Item 1", "Item 2", "Item 3"]
+}
+
+// MARK: - UITableViewDataSource
+extension MenuViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                             for: indexPath)
+    cell.textLabel?.text = items[indexPath.row]
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension MenuViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView,
+                 didSelectRowAt indexPath: IndexPath) {
+    delegate?.menuViewController(self, didSelectItemAtIndex: indexPath.row)
+  }
+}
