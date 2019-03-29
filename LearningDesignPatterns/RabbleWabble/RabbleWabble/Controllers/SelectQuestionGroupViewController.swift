5cf57b05ccdb9d07d1a7b77fc79cd800bae12a8d
tan-ziyu <tan_ziyu@apple.com>
2019-03-29 10:47:57 +0800

    delegate pattern

---
+++ LearningDesignPatterns/RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SelectQuestionGroupViewController.swift
+//  RabbleWabble
+//
+//  Created by ATLAS on 2019/3/28.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
+}
+
+// MARK: - UITableViewDataSource
+extension SelectQuestionGroupViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionGroups[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SelectQuestionGroupViewController: UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionGroups[indexPath.row]
+        return indexPath
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else { return }
+        viewController.questionGroup = selectedQuestionGroup
+    }
+}
---
+++ LearningDesignPatterns/RabbleWabble/RabbleWabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble
+//
+//  Created by ATLAS on 2019/3/28.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
