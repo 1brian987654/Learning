7c83cfc13c13dd5cfb199d3a76d8f4f80380c8fc
tan-ziyu <tan_ziyu@apple.com>
2019-04-09 19:19:13 +0800

    temp for delegation pattern

---
+++ LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Controllers/SelectQuestionGroupViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SelectQuestionGroupViewController.swift
+//  RabbleWabble02
+//
+//  Created by ATLAS on 2019/4/9.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
+}
+
+// MARK: - UITableViewDataSource
+extension SelectQuestionGroupViewController: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionGroups[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
+}
+
+// MARK: -
---
+++ LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Views/QuestionGroupCell.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble02
+//
+//  Created by ATLAS on 2019/4/9.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
