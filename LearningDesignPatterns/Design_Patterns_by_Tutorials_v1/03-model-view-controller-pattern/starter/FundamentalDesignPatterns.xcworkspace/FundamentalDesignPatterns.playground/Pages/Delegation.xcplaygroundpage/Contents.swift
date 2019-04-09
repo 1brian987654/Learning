022a10d84d51364b5ae598bd47879da8555d3556
tan-ziyu <tan_ziyu@apple.com>
2019-04-09 16:30:08 +0800

    temp commit

--- LearningDesignPatterns/Design_Patterns_by_Tutorials_v1/03-model-view-controller-pattern/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
+++ LearningDesignPatterns/Design_Patterns_by_Tutorials_v1/03-model-view-controller-pattern/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
@@ -11,3 +11,46 @@
  
  ## Code Example
  */
+import UIKit
+
+protocol MenuViewControllerDelegate {
+    func menuViewController(_ menuViewController: MenuViewController, didSelectItemAtIndex index: Int)
+}
+
+class MenuViewController: UIViewController {
+    
+    var delegate: MenuViewControllerDelegate?
+    
+    // 1
+    @IBOutlet var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+    
+    // 2
+    let items = ["Item 1", "Item 2", "Item 3"]
+}
+
+// MARK: - UITableViewDataSource
+extension MenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // It's common convention to pass the delegating object to each of its delegate method calls.
+        // This way, the delegate can use or inspect the caller if needed.
+        delegate?.menuViewController(self, didSelectItemAtIndex: indexPath.row)
+    }
+}
---
+++ LearningDesignPatterns/RabbleWabble_01_MVC/RabbleWabble02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RabbleWabble02
+//
+//  Created by ATLAS on 2019/4/4.
+//  Copyright © 2019 Brian. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
