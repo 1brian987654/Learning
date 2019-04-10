4764c2082969156b2608c200810ef09b5933719d
tan-ziyu <tan_ziyu@apple.com>
2019-04-10 15:29:20 +0800

    page 76, 2. You also can't see how many questions are remaining in the

--- LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Controllers/QuestionViewController.swift
+++ LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Controllers/QuestionViewController.swift
@@ -12,11 +12,23 @@ class QuestionViewController: UIViewController {
     
     // MARK: - Instance Properties
     // Model
-    public var questionGroup = QuestionGroup.basicPhrases()
+    //public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionGroup: QuestionGroup! {
+        didSet {
+            navigationItem.title = questionGroup.title
+        }
+    }
     public var questionIndex = 0
     
     public var correctCount = 0
     public var incorrectCount = 0
+    
+    private lazy var questionIndexItem: UIBarButtonItem = {
+        let item = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        item.tintColor = .black
+        navigationItem.rightBarButtonItem = item
+        return item
+    }()
     
     // View
     public var questionView: QuestionView! {
@@ -43,6 +55,8 @@ class QuestionViewController: UIViewController {
         
         questionView.answerLabel.isHidden = true
         questionView.hintLabel.isHidden = true
+        
+        questionIndexItem.title = "\(questionIndex + 1)/\(questionGroup.questions.count)"
     }
     
     @IBAction func toggleAnswerLabels(_ sender: Any) {
--- LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Controllers/SelectQuestionGroupViewController.swift
+++ LearningDesignPatterns/RabbleWabble_02_Delegation/RabbleWabble02/Controllers/SelectQuestionGroupViewController.swift
@@ -37,4 +37,33 @@ extension SelectQuestionGroupViewController: UITableViewDataSource {
     }
 }
 
-// MARK: -
+// MARK: - UITableViewDelegate
+extension SelectQuestionGroupViewController: UITableViewDelegate {
+    
+    /* This sets the selectedQuestionGroup to the one that was selected.
+     You have to do this here instead of in tableView(_:, didSelectRowAt:),
+     because didSelectRowAt: is triggered after the segue is performed.
+     If you set selectedQuestionGroup in didSelectRowAt: then the app would crash on the line
+     viewController.questionGroup = selectedQuestionGroup
+     as selectedQuestionGroup would still be nil then. */
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionGroups[indexPath.row]
+        return indexPath
+    }
+    
+    /* Within this function, you simply deselect the table view cell.
+     This is just a nicety so you won't see any selected cells should you return to this view controller later. */ 
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    /* Within this function, you guard that the segue.
+     destination is actually a QuestionViewController (just in case!),
+     and if so, you set its questionGroup to the selectedQuestionGroup. */
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else {
+            return
+        }
+        viewController.questionGroup = selectedQuestionGroup
+    }
+}
